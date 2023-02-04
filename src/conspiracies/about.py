b02c70f2dae5d792203f5b56068b2d8e932dc562
GitHub Action <action@github.com>
2023-02-04 20:03:05 +0000

    Add changes for f6afdc3adf6c748a8daab96a75aa12141e240774

--- src/conspiracies/about.py
+++
@@ -1,6 +0,0 @@
-from importlib.metadata import version
-
-
-__title__ = "conspiracies"
-__version__ = version(__title__)
-__download_url__ = "https://github.com/centre-for-humanities-computing/conspiracies"
