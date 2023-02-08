343b95342e178b9f5d4bdbc64b88d4327d0fe9e6
stinenyhus <stnyl@hotmail.com>
2023-02-08 14:55:23 +0100

    add argparse for triplet extraction and output printing

--- paper/Triplets-Extraction-GPT/src/extract_triplets.py
+++ paper/Triplets-Extraction-GPT/src/extract_triplets.py
@@ -10,7 +10,8 @@ from create_templates import (
     prompt_template_3,
     prompt_template_4,
 )
-
+from typing import List
+import argparse
 
 def create_list_dics(tweet, triplets):
     result = {}
@@ -18,27 +19,24 @@ def create_list_dics(tweet, triplets):
     result["triplets"] = triplets
     return result
 
+def get_all_prompt_functions():
+    return {
+        "template1": prompt_template_1,
+        "template2": prompt_template_2,
+        "template3": prompt_template_3,
+        "template4": prompt_template_4,
+    }
 
-root_path = os.path.join("/work", "conspiracies", "data", "triplet-extraction-gpt")
-with open(
-    os.path.join(root_path, "tagged", "tagged_tweets_with_features.json"),
-    "r",
-    encoding="utf8",
-) as f:
-    data = json.load(f)
-with open(os.path.join("/work", "conspiracies", "openai_API_key.txt")) as f:
-    openai_key = f.read()
-
-
-dict_functions = {
-    #   "template1": prompt_template_1,
-    #   "template2": prompt_template_2,
-    "template3": prompt_template_3,
-    "template4": prompt_template_4,
-}
-
-for i in range(2):
-    examples, target_tweets = extract_examples(data, 9)
+def run_triplet_extraction(
+        data:List[dict], 
+        prediction_path:str,
+        n_tweets:int, 
+        dict_functions:dict, 
+        openai_key:str,
+        iteration:int):
+    
+    print(f"Running function with {n_tweets} tweets")
+    examples, target_tweets = extract_examples(data, n_tweets)
 
     examples_set = []
     for tweet, triplet in examples:
@@ -47,24 +45,27 @@ for i in range(2):
 
     examples_json = json.dumps(examples_set, ensure_ascii=False, indent=2)
 
-    with open(
-        os.path.join(root_path, "predictions", f"examples_set_{i}.json"), "w"
-    ) as outfile:
+    with open(os.path.join(prediction_path, 
+                           f"examples_set_{iteration}.json"), "w") as outfile:
         outfile.write(examples_json)
 
     for key, value in dict_functions.items():
-        print(f"Using {key}\n")
+        print(f"Prompting using {key}\n")
 
         gpt_outputs = []
         for tweet in target_tweets:
             template = value(
                 examples=examples,
                 target_tweet=tweet,
-                introduction="Extract semantic triples from the following tweets and put them in the form (Subject)(VerbPhrase)(Arg):",
+                introduction=
+                """Extract semantic triplets from the following tweet. 
+The semantic triplets should be on the form (Subject - Verb Phrase - Object), where the verb phrase includes all particles and modifyers. 
+There should always be exactly three phrases extracted, no more no less. 
+They should be put in a markdown table as shown below:""",
             )
-            print("Template used:\n\n")
-            print(template)
-            print("\n\n")
+            # print("Template used:\n\n")
+            # print(template)
+            # print("\n\n")
             openai.api_key = openai_key
             response = openai.Completion.create(
                 model="text-davinci-002",
@@ -76,7 +77,81 @@ for i in range(2):
             result = create_list_dics(tweet, response["choices"][0]["text"])
             gpt_outputs.append(result)
         outputs_json = json.dumps(gpt_outputs, ensure_ascii=False, indent=2)
-        with open(
-            os.path.join(root_path, "predictions", f"{key}_gpt_outputs_{i}.json"), "w"
-        ) as outfile:
+
+        with open(os.path.join(prediction_path, 
+                               f"{key}_gpt_outputs_{iteration}.json"), "w") as outfile:
             outfile.write(outputs_json)
+
+
+def main(machine:str,
+        n_tweets:int, 
+        templates:List[int],
+        iterations:int
+        ):
+
+    # Preparing paths, files and folders
+    if machine == 'grundtvig':
+        root_path = os.path.join("/data", "conspiracies", "triplet-extraction-gpt")
+        prediction_path = os.path.join("/home", 
+                                       os.getlogin(), 
+                                       "data", 
+                                       "predictions")
+        with open(os.path.join("/home", os.getlogin(), "openai_API_key.txt")) as f:
+            openai_key = f.read()
+    elif machine == 'ucloud':
+        root_path = os.path.join("/work", "conspiracies", "data", "triplet-extraction-gpt")
+        prediction_path = os.path.join(root_path, "predictions")
+        with open(os.path.join("/work", "conspiracies", "openai_API_key.txt")) as f:
+            openai_key = f.read()
+    else:
+        root_path = os.getcwd()
+        openai_key = "key"
+        prediction_path = root_path
+        print(f'Invalid machine, using current path ({root_path}) and the openai key "{openai_key}"')
+
+    with open(os.path.join(root_path, "tagged", "tagged_tweets_with_features.json"),
+              "r",
+              encoding="utf8",) as f:
+        data = json.load(f)
+    
+    all_dicts = get_all_prompt_functions()
+    dict_functions = {f'template{i}':all_dicts[f'template{i}'] for i in templates}
+
+    # Looping over triplet extraction, exception for too long prompt decreases n example tweets
+    for i in range(iterations):
+        print(f'Iteration {i}')
+
+        while True:
+            try:
+                run_triplet_extraction(data, prediction_path, n_tweets, dict_functions, openai_key, i)
+                break
+            except openai.error.InvalidRequestError:
+                n_tweets -= 1
+                continue
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m',
+                        '--machine', 
+                        type=str,
+                        help='Which machine are you working on? Takes values "grundtvig" or "ucloud')
+    parser.add_argument('-n',
+                        '--n_tweets', 
+                        default=20,
+                        type=int,
+                        help='Number of example tweets to use if possible')
+    parser.add_argument('-t',
+                        '--templates',
+                        nargs='+',
+                        type=int,
+                        help='Integers indicating templates to use')
+    parser.add_argument('-i',
+                        '--iterations',
+                        default=2,
+                        type=int,
+                        help='Number of iterations of sampling and prompting')
+
+    args = parser.parse_args()
+    
+    main(args.machine, args.n_tweets, args.templates, args.iterations)
+    
--- paper/Triplets-Extraction-GPT/src/print_gpt_output_compare_prompts.py
+++ paper/Triplets-Extraction-GPT/src/print_gpt_output_compare_prompts.py
@@ -1,6 +1,7 @@
 import json
 import os
-
+from typing import List
+import argparse
 
 def find_tweet_in_list_of_dicts(tweet, dict_list):
     for d in dict_list:
@@ -14,68 +15,113 @@ def get_header(template_number, tweet):
     else:
         return "| Subject | Predicate | Object |\n| --- | --- | --- |\n"
 
+def main(machine: str, 
+         print_in_terminal:bool, 
+         templates:List[int], 
+         iterations:int):
+    if machine == 'grundtvig':
+        root_path = os.path.join("/data", "conspiracies", "triplet-extraction-gpt")
+        prediction_path = os.path.join("/home", 
+                                       os.getlogin(), 
+                                       "data", 
+                                       "predictions")
+    elif machine == 'ucloud':
+        root_path = os.path.join("/work", "conspiracies", "data", "triplet-extraction-gpt")
+        prediction_path = os.path.join(root_path, "predictions")
+    else:
+        root_path = os.getcwd()
+        prediction_path = root_path
+        print(f'Invalid machine, using current path ({root_path})"')
+    
+    out_file = os.path.join(prediction_path, "prompt_outputs_compare_templates.txt")
 
-root_path = os.path.join("/work", "conspiracies", "data", "triplet-extraction-gpt")
-template_numbers = [3, 4]
-out_file = "prompt_outputs_compare_templates.txt"
+    with open(out_file, "w") as f:
+        f.write("Comparing templates:\n")
+        for n in templates:
+            f.write(f"\t{n}\n")
 
-with open(out_file, "w") as f:
-    f.write("Comparing templates:\n")
-    for n in template_numbers:
-        f.write(f"\t{n}\n")
-
-
-for i in range(2):  # This should really be automized
-    with open(out_file, "a") as f:
-        f.write(f"\n### Examples set {i}\n")
-    first = template_numbers[0]
-    first_file = os.path.join(
-        root_path, "predictions", f"template{first}_gpt_outputs_{i}.json"
-    )
-    gold_file = os.path.join(root_path, "tagged", "tagged_tweets_with_features.json")
-
-    with open(first_file, "r") as f:
-        first_data = json.load(f)
-
-    with open(gold_file, "r") as f:
-        input_data = json.load(f)
-
-    for d in first_data:
-        tweet = d["tweet"]
-        gold_dict = find_tweet_in_list_of_dicts(tweet, input_data)
-        input_text = f"\n\n#### Target tweet:\n{tweet}\n\n##### Gold:\n"
-        gold_header = get_header(0, tweet)  # Any number but 3
-        first_header = get_header(first, tweet)
-        print(input_text)
-        print(gold_header)
+    for i in range(iterations):
         with open(out_file, "a") as f:
-            f.write(input_text)
-            f.write(gold_header)
+            f.write(f"\n### Examples set {i}\n")
+        first = templates[0]
+        first_file = os.path.join(
+            prediction_path, f"template{first}_gpt_outputs_{i}.json"
+        )
+        gold_file = os.path.join(root_path, "tagged", "tagged_tweets_with_features.json")
 
-        for triplet in gold_dict["triplets"]:
-            triplet_string = f"|{triplet[0]}|{triplet[1]}|{triplet[2]}|\n"
+        with open(first_file, "r") as f:
+            first_data = json.load(f)
+
+        with open(gold_file, "r") as f:
+            input_data = json.load(f)
+
+        for d in first_data:
+            tweet = d["tweet"]
+            gold_dict = find_tweet_in_list_of_dicts(tweet, input_data)
+            input_text = f"\n\n#### Target tweet:\n{tweet}\n\n##### Gold:\n"
+            gold_header = get_header(0, tweet)  # Any number but 3
+            first_header = get_header(first, tweet)
+
+            if print_in_terminal:
+                print(input_text)
+                print(gold_header)
             with open(out_file, "a") as f:
-                f.write(triplet_string)
+                f.write(input_text)
+                f.write(gold_header)
 
-        with open(out_file, "a") as f:
-            f.write(f"\n##### Template {first}:\n")
-            f.write(first_header)
-            f.write(d["triplets"])
-        print(f"\n\nTemplate {first}:\n")
-        print(first_header, d["triplets"])
+            for triplet in gold_dict["triplets"]:
+                triplet_string = f"|{triplet[0]}|{triplet[1]}|{triplet[2]}|\n"
+                with open(out_file, "a") as f:
+                    f.write(triplet_string)
 
-        for n in template_numbers[1:]:
-            file = os.path.join(
-                root_path, "predictions", f"template{n}_gpt_outputs_{i}.json"
-            )
-            n_header = get_header(n, tweet)
-            with open(file, "r") as f:
-                data = json.load(f)
-            new_dict = find_tweet_in_list_of_dicts(tweet, data)
             with open(out_file, "a") as f:
-                f.write(f"\n\n##### Template {n}:\n")
-                f.write(n_header)
-                f.write(new_dict["triplets"])
-            print(f"\nTemplate {n}:\n")
-            print(n_header)
-            print(new_dict["triplets"])
+                f.write(f"\n##### Template {first}:\n")
+                f.write(first_header)
+                f.write(d["triplets"])
+
+            if print_in_terminal:
+                print(f"\n\nTemplate {first}:\n")
+                print(first_header, d["triplets"])
+
+            for n in templates[1:]:
+                new_file = os.path.join(
+                    prediction_path, f"template{n}_gpt_outputs_{i}.json"
+                )
+                n_header = get_header(n, tweet)
+                with open(new_file, "r") as f:
+                    data = json.load(f)
+                new_dict = find_tweet_in_list_of_dicts(tweet, data)
+                with open(out_file, "a") as f:
+                    f.write(f"\n\n##### Template {n}:\n")
+                    f.write(n_header)
+                    f.write(new_dict["triplets"])
+                
+                if print_in_terminal:
+                    print(f"\nTemplate {n}:\n")
+                    print(n_header)
+                    print(new_dict["triplets"])
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m',
+                        '--machine', 
+                        type=str,
+                        help='Which machine are you working on? Takes values "grundtvig" or "ucloud')
+    parser.add_argument('-p',
+                        '--print_terminal', 
+                        default=False,
+                        type=bool,
+                        help='Whether or not to also print directly to console')
+    parser.add_argument('-t',
+                        '--templates',
+                        nargs='+',
+                        type=int,
+                        help='Integers indicating templates to use')
+    parser.add_argument('-i',
+                        '--iterations',
+                        default=2,
+                        type=int,
+                        help='Number of iterations of sampling and prompting')
+    args = parser.parse_args()
+
+    main(args.machine, args.print_terminal, args.templates, args.iterations)
