ce6666655d9c110216ba1c034d37fc8636e2ff0e
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2023-02-23 14:43:47 +0100

    feat: Added gold data to package data

---
+++ src/conspiracies/data/load.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from typing import List, Tuple
+
+import spacy
+from spacy.tokens import Doc
+
+from conspiracies import SpanTriplet, docs_from_jsonl
+
+
+def load_gold_triplets() -> Tuple[List[Doc], List[List[SpanTriplet]]]:
+    nlp = spacy.blank("en")
+    nlp.add_pipe("sentencizer")
+    path = Path(__file__).parent / "gold_triplets.jsonl"
+    docs, triplets = docs_from_jsonl(path, nlp)
+    return docs, triplets
