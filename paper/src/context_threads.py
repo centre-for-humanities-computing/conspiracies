070412c283b4d32fba1a18002ff9d2911d7b6bd0
thearol <thearollesloth@hotmail.com>
2023-03-27 16:44:30 +0200

    optimize twitter_threads

--- paper/src/context_threads.py
+++ paper/src/context_threads.py
@@ -15,8 +15,12 @@ def ndjson_gen(filepath: str):
         with open(file) as f:
             reader = ndjson.reader(f)
             for post in reader:
+                
+                if re.search("^RT", post["text"]):  # remove retweets
+                    continue
                 yield post
 
+'''
 ### context window thread ###
 def context_window_thread(tweets, context_len, start_date, extra_days):
     """
@@ -32,8 +36,7 @@ def context_window_thread(tweets, context_len, start_date, extra_days):
     conversations: dict = {}
     # loop over tweets and create conversation dicts
     for tweet in tweets:
-        if re.search("^RT", tweet["text"]):  # remove retweets
-            continue
+        
         conv_id = tweet["conversation_id"]
         if conv_id not in conversations.keys():
             conversations[conv_id] = {tweet["id"]: tweet}
@@ -55,13 +58,15 @@ def context_window_thread(tweets, context_len, start_date, extra_days):
                 return [tweet]
             else:
                 if "referenced_tweets" not in tweet.keys():
-                    raise Warning("reply but no reference?") #googl
+                    #raise Warning("reply but no reference?") #googl
+                    print("reply but no reference?") 
                     return [tweet]
 
                 reply_to_id = find_reply_to_id(tweet)
                 try:
                     reply_to = conversation[reply_to_id]
                 except KeyError:
+                    
                     return [tweet]
 
                 context = find_context(
@@ -84,6 +89,51 @@ def context_window_thread(tweets, context_len, start_date, extra_days):
             print("context length too long:", len(context))
             break
     return contexts
+'''
+def context_window_thread(tweets, context_len):
+    """
+    Extracts a thread of context tweets; the last tweet in the thread is the one that the context relates to
+    Args:
+        tweets: list of tweets
+        context_len: number of tweets to include in context
+        start_date: start date of the date range
+        extra_days: number of days to include from start date and after
+    Returns:
+        contexts: list of contexts, where each context is a list of tweets
+    """
+    conversations = {}
+    for tweet in tweets:
+        conv_id = tweet["conversation_id"]
+        conversations.setdefault(conv_id, {})
+        conversations[conv_id][tweet["id"]] = tweet
+
+    def find_context(tweet, conversation, context_length=context_len):
+        if "in_reply_to_user_id" not in tweet:
+            return [tweet]
+
+        if context_length == 0 or "referenced_tweets" not in tweet:
+            return [tweet]
+
+        replied_to_refs = [ref for ref in tweet["referenced_tweets"] if ref["type"] == "replied_to"]
+        if replied_to_refs:
+            reply_to_id = next(ref["id"] for ref in replied_to_refs)
+            try:
+                reply_to = conversation[reply_to_id]
+            except KeyError:
+                return [tweet]
+
+            context = find_context(reply_to, conversation, context_length=context_length - 1)
+            context.append(tweet)
+            return context
+        else:
+            # handle the case where no replied_to reference is found
+            return [tweet]
+
+    contexts = [find_context(tweet, conversations[tweet["conversation_id"]]) for tweet in tweets if len(find_context(tweet, conversations[tweet["conversation_id"]])) > 1]
+    return contexts
+
+
+
 
 def range_of_dates(start_date, extra_days):
     start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d').date()
@@ -107,16 +157,25 @@ if __name__ == "__main__":
     tweets = []
     
     for i, post in enumerate(ndjson_gen(path)):
+        # get the date of the tweet and append the tweet if it is within the date range
         date_ = datetime.datetime.strptime(post["created_at"][0:10], '%Y-%m-%d').date()
-        # append the tweet if it is within the date range
         if date_ in date_range:
             tweets.append(post)    
-    
+
     print(f"Found {len(tweets)} tweets in the date range (out of {i} tweets total)")
-    
+    '''
     #run the tweet thread extraction 
     contexts = context_window_thread(tweets, args.context_len, args.start_date, args.extra_days)
 
     #write contests to ndjson
     with open(os.path.join(f"tweetcontexts_{datetime.datetime.strptime(args.start_date, '%Y-%m-%d').date() }_{datetime.datetime.strptime(args.start_date, '%Y-%m-%d').date() + datetime.timedelta(days=args.extra_days-1)}.ndjson"), "w") as f:
         ndjson.dump(contexts, f) #the ndjson of contexts has the format: List[Dict[str, Any]]
+    '''
+    #CHATGPT
+    #run the tweet thread extraction 
+    gpt_contexts = context_window_thread(tweets, args.context_len)
+
+    #write contests to ndjson
+    with open(os.path.join(f"tweet_threads_{datetime.datetime.strptime(args.start_date, '%Y-%m-%d').date() }_{datetime.datetime.strptime(args.start_date, '%Y-%m-%d').date() + datetime.timedelta(days=args.extra_days-1)}.ndjson"), "w") as f:
+        ndjson.dump(gpt_contexts, f) #the ndjson of contexts has the format: List[Dict[str, Any]]
+    
