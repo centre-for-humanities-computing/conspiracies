4763c00b5deeaad8c9d9d8131800f4c3827b3caf
Kasper Fyhn <kfyhn@cas.au.dk>
2024-11-21 09:33:08 +0100

    visualizer: prettier and switching double-click and hold ops

--- visualizer/src/graph/GraphService.ts
+++ visualizer/src/graph/GraphService.ts
@@ -1,189 +1,200 @@
-import {Edge, GraphData, Node} from "react-vis-graph-wrapper";
+import { Edge, GraphData, Node } from "react-vis-graph-wrapper";
 
 export interface Stats {
-    frequency: number;
-    norm_frequency?: number;
-    docs?: string[];
-    first_occurrence?: string;
-    last_occurrence?: string;
-    alt_labels?: string[];
+  frequency: number;
+  norm_frequency?: number;
+  docs?: string[];
+  first_occurrence?: string;
+  last_occurrence?: string;
+  alt_labels?: string[];
 }
 
 export interface EnrichedNode extends Node {
-    stats: Stats;
+  stats: Stats;
 }
 
 export interface EnrichedEdge extends Edge {
-    stats: Stats;
+  stats: Stats;
 }
 
 export interface EnrichedGraphData extends GraphData {
-    nodes: EnrichedNode[];
-    edges: EnrichedEdge[];
+  nodes: EnrichedNode[];
+  edges: EnrichedEdge[];
 }
 
 export class GraphFilter {
-    minimumNodeFrequency: number;
-    minimumEdgeFrequency: number;
-    earliestDate?: Date;
-    latestDate?: Date;
-    showUnconnectedNodes: boolean = false;
+  minimumNodeFrequency: number;
+  minimumEdgeFrequency: number;
+  earliestDate?: Date;
+  latestDate?: Date;
+  showUnconnectedNodes: boolean = false;
 
-    constructor(minimumNodeFrequency: number = 1, minimumEdgeFrequency: number = 1) {
-        this.minimumNodeFrequency = minimumNodeFrequency;
-        this.minimumEdgeFrequency = minimumEdgeFrequency;
-    }
+  constructor(
+    minimumNodeFrequency: number = 1,
+    minimumEdgeFrequency: number = 1,
+  ) {
+    this.minimumNodeFrequency = minimumNodeFrequency;
+    this.minimumEdgeFrequency = minimumEdgeFrequency;
+  }
 }
 
 function hasDateOverlap(node: EnrichedNode, filter: GraphFilter): boolean {
-    if (!node.stats.first_occurrence || !node.stats.last_occurrence) {
-        return true;
-    }
-    const first = new Date(node.stats.first_occurrence);
-    const last = new Date(node.stats.last_occurrence);
-    const afterEarliestDate = !filter.earliestDate
-        || filter.earliestDate < first
-        || filter.earliestDate < last;
+  if (!node.stats.first_occurrence || !node.stats.last_occurrence) {
+    return true;
+  }
+  const first = new Date(node.stats.first_occurrence);
+  const last = new Date(node.stats.last_occurrence);
+  const afterEarliestDate =
+    !filter.earliestDate ||
+    filter.earliestDate < first ||
+    filter.earliestDate < last;
 
-    const beforeLatestDate = !filter.latestDate
-        || filter.latestDate > first || filter.latestDate > last;
+  const beforeLatestDate =
+    !filter.latestDate || filter.latestDate > first || filter.latestDate > last;
 
-    return afterEarliestDate && beforeLatestDate;
+  return afterEarliestDate && beforeLatestDate;
 }
 
-
-export function filter(filter: GraphFilter, graphData: EnrichedGraphData): EnrichedGraphData {
-    let nodes = graphData.nodes.filter((node: EnrichedNode) =>
-        node.stats.frequency >= filter.minimumNodeFrequency
-        && hasDateOverlap(node, filter)
-    );
-    let filteredNodes = new Set(nodes.map(node => node.id));
-    let edges = graphData.edges.filter((edge: EnrichedEdge) =>
-        edge.stats.frequency >= filter.minimumEdgeFrequency &&
-        filteredNodes.has(edge.from) && filteredNodes.has(edge.to)
-    );
-    let connectedNodes = new Set(edges.flatMap(edge => [edge.from, edge.to]));
-    if (!filter.showUnconnectedNodes) {
-        nodes = nodes.filter(node => connectedNodes.has(node.id));
-    }
-    return {nodes, edges}
+export function filter(
+  filter: GraphFilter,
+  graphData: EnrichedGraphData,
+): EnrichedGraphData {
+  let nodes = graphData.nodes.filter(
+    (node: EnrichedNode) =>
+      node.stats.frequency >= filter.minimumNodeFrequency &&
+      hasDateOverlap(node, filter),
+  );
+  let filteredNodes = new Set(nodes.map((node) => node.id));
+  let edges = graphData.edges.filter(
+    (edge: EnrichedEdge) =>
+      edge.stats.frequency >= filter.minimumEdgeFrequency &&
+      filteredNodes.has(edge.from) &&
+      filteredNodes.has(edge.to),
+  );
+  let connectedNodes = new Set(edges.flatMap((edge) => [edge.from, edge.to]));
+  if (!filter.showUnconnectedNodes) {
+    nodes = nodes.filter((node) => connectedNodes.has(node.id));
+  }
+  return { nodes, edges };
 }
 
 export abstract class GraphService {
-    private nodesMap: Map<string, EnrichedNode> | null = null;
+  private nodesMap: Map<string, EnrichedNode> | null = null;
 
-    abstract getGraph(): EnrichedGraphData;
+  abstract getGraph(): EnrichedGraphData;
 
-    getSubGraph(nodeIds: Set<string>): EnrichedGraphData {
-        return {
-            nodes: this.getGraph().nodes.filter((n: EnrichedNode) => nodeIds.has(n.id!.toString())),
-            edges: this.getGraph().edges
-        }
+  getSubGraph(nodeIds: Set<string>): EnrichedGraphData {
+    return {
+      nodes: this.getGraph().nodes.filter((n: EnrichedNode) =>
+        nodeIds.has(n.id!.toString()),
+      ),
+      edges: this.getGraph().edges,
+    };
+  }
+
+  getConnectedNodes(nodeId: string): Set<string> {
+    return new Set(
+      this.getGraph()
+        .edges.filter((edge) => edge.from === nodeId || edge.to === nodeId)
+        .flatMap((edge) => [edge.from!.toString(), edge.to!.toString()]),
+    );
+  }
+
+  getNode(nodeId: string): EnrichedNode | undefined {
+    if (this.nodesMap === null) {
+      this.nodesMap = new Map(
+        this.getGraph().nodes.map((node) => [node.id!.toString(), node]),
+      );
     }
 
-    getConnectedNodes(nodeId: string): Set<string> {
-        return new Set(this.getGraph().edges.filter(edge => edge.from === nodeId || edge.to === nodeId)
-            .flatMap(edge => [edge.from!.toString(), edge.to!.toString()]))
+    // highly inefficient linear search; overwrite for actual use
+    for (let node of this.getGraph().nodes) {
+      if (node.id === nodeId) {
+        return node;
+      }
     }
-
-    getNode(nodeId: string): EnrichedNode | undefined {
-        if (this.nodesMap === null) {
-            this.nodesMap = new Map(
-                this.getGraph().nodes.map(node => [node.id!.toString(), node])
-            )
-        }
-
-        // highly inefficient linear search; overwrite for actual use
-        for (let node of this.getGraph().nodes) {
-            if (node.id === nodeId) {
-                return node;
-            }
-        }
-        return undefined;
-    }
-
+    return undefined;
+  }
 }
 
-
 export class SampleGraphService extends GraphService {
-    readonly sampleGraphData: EnrichedGraphData = {
-        nodes: [
-            {
-                id: "1",
-                label: "node 1",
-                stats: {
-                    frequency: 3,
-                },
-            },
-            {
-                id: "2",
-                label: "node 2",
-                stats: {
-                    frequency: 2,
-                },
-            },
-            {
-                id: "3",
-                label: "node 3",
-                stats: {
-                    frequency: 2,
-                },
-            },
-            {
-                id: "4",
-                label: "node 4",
-                stats: {
-                    frequency: 1,
-                },
-            },
-        ],
-        edges: [
-            {
-                from: "1",
-                to: "2",
-                stats: {
-                    frequency: 2,
-                },
-            },
-            {
-                from: "1",
-                to: "3",
-                stats: {
-                    frequency: 2,
-                },
-            },
-            {
-                from: "1",
-                to: "4",
-                stats: {
-                    frequency: 1,
-                },
-            },
-            {
-                from: "2",
-                to: "3",
-                stats: {
-                    frequency: 2,
-                },
-            },
-        ],
-    };
+  readonly sampleGraphData: EnrichedGraphData = {
+    nodes: [
+      {
+        id: "1",
+        label: "node 1",
+        stats: {
+          frequency: 3,
+        },
+      },
+      {
+        id: "2",
+        label: "node 2",
+        stats: {
+          frequency: 2,
+        },
+      },
+      {
+        id: "3",
+        label: "node 3",
+        stats: {
+          frequency: 2,
+        },
+      },
+      {
+        id: "4",
+        label: "node 4",
+        stats: {
+          frequency: 1,
+        },
+      },
+    ],
+    edges: [
+      {
+        from: "1",
+        to: "2",
+        stats: {
+          frequency: 2,
+        },
+      },
+      {
+        from: "1",
+        to: "3",
+        stats: {
+          frequency: 2,
+        },
+      },
+      {
+        from: "1",
+        to: "4",
+        stats: {
+          frequency: 1,
+        },
+      },
+      {
+        from: "2",
+        to: "3",
+        stats: {
+          frequency: 2,
+        },
+      },
+    ],
+  };
 
-    getGraph(): EnrichedGraphData {
-        return this.sampleGraphData;
-    }
+  getGraph(): EnrichedGraphData {
+    return this.sampleGraphData;
+  }
 }
 
 export class FileGraphService extends GraphService {
-    private readonly data: EnrichedGraphData = {nodes: [], edges: []};
+  private readonly data: EnrichedGraphData = { nodes: [], edges: [] };
 
-    constructor(data: EnrichedGraphData) {
-        super();
-        this.data = data;
-    }
+  constructor(data: EnrichedGraphData) {
+    super();
+    this.data = data;
+  }
 
-    getGraph(): EnrichedGraphData {
-        return this.data;
-    }
-
+  getGraph(): EnrichedGraphData {
+    return this.data;
+  }
 }
--- visualizer/src/graph/GraphViewer.tsx
+++ visualizer/src/graph/GraphViewer.tsx
@@ -1,132 +1,158 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
-    EnrichedGraphData,
-    EnrichedNode,
-    FileGraphService,
-    filter,
-    GraphFilter,
-    GraphService,
-    SampleGraphService,
+  EnrichedGraphData,
+  EnrichedNode,
+  FileGraphService,
+  filter,
+  GraphFilter,
+  GraphService,
+  SampleGraphService,
 } from "./GraphService";
 import FileUploadComponent from "../datasources/FileUploadComp";
-import Graph, {GraphEvents, Options} from "react-vis-graph-wrapper";
-import {GraphFilterControlPanel} from "./GraphFilterControlPanel";
-import {GraphOptionsControlPanel} from "./GraphOptionsControlPanel";
-import {NodeInfo} from "./NodeInfo";
-
+import Graph, { GraphEvents, Options } from "react-vis-graph-wrapper";
+import { GraphFilterControlPanel } from "./GraphFilterControlPanel";
+import { GraphOptionsControlPanel } from "./GraphOptionsControlPanel";
+import { NodeInfo } from "./NodeInfo";
 
 export const GraphViewer: React.FC = () => {
-    let graphServiceRef = useRef<GraphService>(new SampleGraphService());
-    const [graphData, setGraphData] = useState(
-        graphServiceRef.current.getGraph()
-    );
+  let graphServiceRef = useRef<GraphService>(new SampleGraphService());
+  const [graphData, setGraphData] = useState(
+    graphServiceRef.current.getGraph(),
+  );
 
-    const handleFileLoaded = (data: any) => {
-        graphServiceRef.current = new FileGraphService(data);
-        setGraphData(filter(graphFilter, graphServiceRef.current.getGraph()));
-    };
+  const handleFileLoaded = (data: any) => {
+    if (data === "SAMPLE") {
+      graphServiceRef.current = new SampleGraphService();
+      setGraphFilter(new GraphFilter(1, 0));
+    } else {
+      graphServiceRef.current = new FileGraphService(data);
+      const top50 =
+        graphServiceRef.current
+          .getGraph()
+          .nodes.map((n) => n.stats.frequency)
+          .sort()
+          .reverse()
+          .at(100) || 1;
+      setGraphFilter(new GraphFilter(top50, Math.floor(top50 / 2)));
+    }
+    setGraphData(filter(graphFilter, graphServiceRef.current.getGraph()));
+  };
 
-    const [graphFilter, setGraphFilter] = useState(new GraphFilter(5, 3))
-    const [selected, setSelected] = useState(new Set<string>())
-    const [selectedNode, setSelectedNode] = useState<EnrichedNode | undefined>(undefined)
+  const [graphFilter, setGraphFilter] = useState(new GraphFilter(5, 3));
+  const [selected, setSelected] = useState(new Set<string>());
+  const [selectedNode, setSelectedNode] = useState<EnrichedNode | undefined>(
+    undefined,
+  );
 
-    useEffect(
-        () => {
-            let newGraphData: EnrichedGraphData;
-            if (selected.size > 0) {
-                newGraphData = graphServiceRef.current.getSubGraph(selected);
-            } else {
-                newGraphData = graphServiceRef.current.getGraph();
-            }
-            setGraphData(filter(graphFilter, newGraphData))
-        },
-        [graphFilter, selected]
-    )
+  useEffect(() => {
+    let newGraphData: EnrichedGraphData;
+    if (selected.size > 0) {
+      newGraphData = graphServiceRef.current.getSubGraph(selected);
+    } else {
+      newGraphData = graphServiceRef.current.getGraph();
+    }
+    setGraphData(filter(graphFilter, newGraphData));
+  }, [graphFilter, selected]);
 
-    let events: GraphEvents = {
-        doubleClick: ({nodes}) => {
-            const newSelected = new Set(selected);
-            nodes.forEach((element: string) => {
-                newSelected.delete(element);
-            });
-            setSelected(newSelected);
-        },
-        select: ({nodes}) => {
-            let newSelected: Set<string>;
-            if (nodes.length > 1) {
-                newSelected = new Set<string>();
-                nodes.forEach((element: string) => {
-                    newSelected.add(element);
-                });
-                setSelected(newSelected);
-            }
-        },
-        hold: ({nodes}) => {
-            const newSelected = new Set(selected);
-            nodes.forEach((element: string) => {
-                Array.from(graphServiceRef.current.getConnectedNodes(element)).forEach(c => newSelected.add(c))
-            });
-            setSelected(newSelected);
-        },
-        selectNode: ({nodes}) => {
-            setSelectedNode(graphServiceRef.current.getNode(nodes[0]));
-        },
-        deselectNode: () => {
-            setSelectedNode(undefined);
-        }
-    };
+  let events: GraphEvents = {
+    hold: ({ nodes }) => {
+      const newSelected = new Set(selected);
+      nodes.forEach((element: string) => {
+        newSelected.delete(element);
+      });
+      setSelected(newSelected);
+    },
+    select: ({ nodes }) => {
+      let newSelected: Set<string>;
+      if (nodes.length > 1) {
+        newSelected = new Set<string>();
+        nodes.forEach((element: string) => {
+          newSelected.add(element);
+        });
+        setSelected(newSelected);
+      }
+    },
+    doubleClick: ({ nodes }) => {
+      const newSelected = new Set(selected);
+      nodes.forEach((element: string) => {
+        Array.from(graphServiceRef.current.getConnectedNodes(element)).forEach(
+          (c) => newSelected.add(c),
+        );
+      });
+      setSelected(newSelected);
+    },
+    selectNode: ({ nodes }) => {
+      setSelectedNode(graphServiceRef.current.getNode(nodes[0]));
+    },
+    deselectNode: () => {
+      setSelectedNode(undefined);
+    },
+  };
 
-    let [options, setOptions] = useState<Options>({
-        physics: {
-            enabled: true,
-            barnesHut: {
-                springLength: 200
-            }
-        },
-        edges: {
-            smooth: false,
-            font: {
-                align: 'top'
-            }
-        }
-    })
+  let [options, setOptions] = useState<Options>({
+    physics: {
+      enabled: true,
+      barnesHut: {
+        springLength: 200,
+      },
+    },
+    edges: {
+      smooth: false,
+      font: {
+        align: "top",
+      },
+    },
+  });
 
-    return (
-        <div>
-
-            <div className={"padded"}>
-                <FileUploadComponent onFileLoaded={handleFileLoaded}/>
-            </div>
-            <div className={"padded"}>
-                <GraphFilterControlPanel graphFilter={graphFilter} setGraphFilter={setGraphFilter}/>
-                <GraphOptionsControlPanel options={options} setOptions={setOptions}/>
-            </div>
-            <div className={"padded"}>
-                <div className={"flex-container"}>
-                    <div className={"flex-container__element"}>
-                        <span><b>Shift+select</b> to show subgraph.</span>
-                    </div>
-                    <div className={"flex-container__element"}>
-                        <span><b>Double-click</b> node to remove it.</span>
-                    </div>
-                    <div className={"flex-container__element"}>
-                        <span><b>Hold</b> to expand from node.</span>
-                    </div>
-                    <button className={"flex-container__element"} onClick={() => {
-                        setGraphData(filter(graphFilter, graphServiceRef.current.getGraph()));
-                        setSelected(new Set<string>());
-                    }}>
-                        Reset selection
-                    </button>
-                </div>
-
-
-            </div>
-            <div className="graph-container">
-                {selectedNode && <NodeInfo node={selectedNode}/>}
-
-                <Graph graph={graphData} options={options} events={events}/>
-            </div>
+  return (
+    <div>
+      <div className={"padded flex-container"}>
+        <FileUploadComponent onFileLoaded={handleFileLoaded} />
+        <button onClick={() => handleFileLoaded("SAMPLE")}>Sample data</button>
+      </div>
+      <hr />
+      <div className={"padded"}>
+        <GraphFilterControlPanel
+          graphFilter={graphFilter}
+          setGraphFilter={setGraphFilter}
+        />
+        <GraphOptionsControlPanel options={options} setOptions={setOptions} />
+      </div>
+      <div className={"padded"}>
+        <div className={"flex-container"}>
+          <div className={"flex-container__element"}>
+            <span>
+              <b>Shift+mark multiple</b> to make subgraph.
+            </span>
+          </div>
+          <div className={"flex-container__element"}>
+            <span>
+              <b>Hold</b> node to remove it.
+            </span>
+          </div>
+          <div className={"flex-container__element"}>
+            <span>
+              <b>Double-click</b> to expand from node.
+            </span>
+          </div>
+          <button
+            className={"flex-container__element"}
+            onClick={() => {
+              setGraphData(
+                filter(graphFilter, graphServiceRef.current.getGraph()),
+              );
+              setSelected(new Set<string>());
+            }}
+          >
+            Reset selection
+          </button>
         </div>
-    );
+      </div>
+      <div className="graph-container">
+        {selectedNode && <NodeInfo node={selectedNode} />}
+
+        <Graph graph={graphData} options={options} events={events} />
+      </div>
+    </div>
+  );
 };
